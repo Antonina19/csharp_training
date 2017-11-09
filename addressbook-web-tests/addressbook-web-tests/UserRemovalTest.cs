ed1e3b69b6a08240e6f76e52d6bc196f52e6b4ca
Antonina <tony-94@bk.ru>
2017-11-09 23:30:13 +0300

    Создание базового класса для всех тестовых классов

---
+++ addressbook-web-tests/addressbook-web-tests/UserRemovalTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+
+namespace WebAddressbookTests 
+{
+    [TestFixture]
+    public class UserRemovalTests : TestBase
+    {
+
+        [Test]
+        public void UserRemovalTest()
+        {
+            GoToHomePage();
+            Login(new AccountData("admin", "secret"));
+            SelectUser();
+            RemoveUser();
+        }
+
+
+
+
+    }
+}
